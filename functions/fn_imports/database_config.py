5fc354af27ff5998a968f695fee1fb10fc6a48db
Sumy Ganas <aksmy.997@gmail.com>
2025-07-28 12:19:37 -0700

    add more integration and unit tests

--- functions/fn_imports/database_config.py
+++ functions/fn_imports/database_config.py
@@ -17,54 +17,36 @@ db = firestore.client()
 BUCKET_NAME = "promo_list_for_ai_scraper_123"
 COLLECTION_NAME = "ai_data_cache"
 
-def write_promos():
+def write_promos(bucket_name_override=None, blob_name_override=None, contents_override=None):
     """Add promos to a cloud storage bucket"""
     storage_client = storage.Client()
     deal_generator = web_scraper.DealGenerator()
 
-    bucket_name = BUCKET_NAME
-    blob_name = "promo-blob"
+    bucket_name = bucket_name_override or BUCKET_NAME
+    blob_name = blob_name_override or "promo-blob"
+    contents = contents_override or deal_generator.get_all_data()
 
     bucket = storage_client.bucket(bucket_name)
 
     try:
-        contents = deal_generator.get_all_data()
         blob = bucket.blob(blob_name)
         blob.upload_from_string(contents)
-        logger.info("New promos written to cloud storage bucket")
     except RuntimeError:
         logger.error("Issue encountered while writing new promos to the bucket")
 
 
-def read_promos():
+def read_promos(bucket_name_override=None, blob_name_override=None):
     """Read promos from a cloud storage bucket"""
     storage_client = storage.Client()
 
-    bucket = storage_client.bucket(BUCKET_NAME)
+    bucket_name = bucket_name_override or BUCKET_NAME
+    blob_name = blob_name_override or "promo-blob"
 
-    blob_name = "promo-blob"
+    bucket = storage_client.bucket(bucket_name)
     blob = bucket.get_blob(blob_name)
     promos = blob.download_as_text()
 
     return promos
-
-def test_cloud_storage_conn():
-    """testing cloud storage database connectivity"""
-    storage_client = storage.Client()
-
-    bucket = storage_client.bucket(BUCKET_NAME)
-
-    blob = bucket.blob("test_blob")
-
-    blob.upload_from_string("This is a test string")
-
-    time.sleep(5)
-
-    data = blob.download_as_text()
-
-    print(data)
-
-
 
 def check_if_cached(query: str):
     """
