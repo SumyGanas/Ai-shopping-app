fbc220a6a0e091be111c9a83454d137def87d6cd
Sumy Ganas <aksmy.997@gmail.com>
2025-08-06 14:41:22 -0700

    fix prompts

--- functions/fn_imports/ai.py
+++ functions/fn_imports/ai.py
@@ -122,7 +122,7 @@ class AiBot():
         try:
             response = client.models.generate_content(
             model=self.model,
-            contents=f"\n{promos}\n\nRecommend the **top 3 makeup, skincare, and haircare items** for someone who has {query[0]} skin with {query[1]}, {query[2]} hair that is {query[3]}, and likes a {query[4]} makeup look from the provided deals, gifts with purchase, and discounts. Provide these details for each reccomended product: product name, why each product is relevant for the customer's preferences, product link, original price, and sale price. Exclude deals with kit prices and value prices.",
+            contents=f"List of promos:\n{promos}\n\nExcluding deals with kit prices and value prices, recommend the **top 3 makeup, skincare, and haircare items** from these products for someone who has {query[0]} skin with {query[1]}, {query[2]} hair that is {query[3]}, and likes a {query[4]} makeup look. Using the the provided deals, gifts with purchase, and discounts, respond with the product name, why each product is relevant for the customer's preferences, product link, original price, and sale price, for each category.",
             config = types.GenerateContentConfig(
                 response_mime_type="application/json",
                 response_schema=self.pref_schema,
@@ -153,13 +153,13 @@ class AiBot():
         try:
             response = client.models.generate_content(
             model=self.model,
-            contents="List of promos:\n{promos}\n\nIdentify the top 10 best deals on beauty products from the provided deals, gifts with purchase, and discounts. Exclude deals with kit prices and value prices. Try combining deals to get the best value for purchase. Provide the following details for each deal: product name, product link, original price, sale price, and a brief deal analysis.",
+            contents="List of promos:\n{promos}\n\nExcluding deals with kit prices and value prices, identify the top 10 best deals on beauty products from the provided deals, gifts with purchase, and discounts. Using the data, respond with the product name, product link, original price, sale price, and a brief deal analysis for each deal.",
             
             config = types.GenerateContentConfig(
                 response_mime_type="application/json",
                 response_schema=self.td_schema,
                 thinking_config=types.ThinkingConfig(thinking_budget=-1),
-                system_instruction="You are an expert in finding product deals and savings."
+                system_instruction="You are an expert in identifying good product deals and combining them to get the best value for purchase."
             ))
             
             clean_response = self.clean_json(response.text)
