0063710262eb0a04da18ef1808b011ce956d6005
Sumy Ganas <aksmy.997@gmail.com>
2025-10-02 12:38:51 -0700

    fix bug

--- functions/fn_imports/web_scraper.py
+++ functions/fn_imports/web_scraper.py
@@ -66,7 +66,7 @@ class DealGenerator():
             obj = item.select("div")[0].text + " " + item.select("div")[2].text     
             promo_list.append(obj)
         
-        pl = promo_list if len(promo_list) else ""
+        pl = promo_list if len(promo_list) else []
         return pl
     
     def __get_td_promos(self) -> list[str]:
@@ -80,7 +80,7 @@ class DealGenerator():
             obj = item.select("div.LargeDealCard__headline")[0].text +" "+ item.select("div.LargeDealCard__subtitle")[0].text 
             promo_list.append(obj)
         
-        pl = promo_list if len(promo_list) else ""
+        pl = promo_list if len(promo_list) else []
         return pl
     
     def __get_bmsm(self) -> list[str]:
@@ -95,7 +95,7 @@ class DealGenerator():
             p1 = re.sub("Online only","",text)
             p2 = re.sub(r"\([^)]*?\)","",p1)   
             promo_list.append(p2)
-        pl = promo_list if len(promo_list) else ""
+        pl = promo_list if len(promo_list) else []
         return pl
     
     def get_sale_data(self, item_type: str) -> list[dict]:
